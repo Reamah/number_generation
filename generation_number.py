415188f22b54decd23f663d595c039de8830e258
Reamah <ezareettah@gmail.com>
2023-06-15 21:15:15 +0300

    Create generation_number.py

---
+++ generation_number.py
@@ -0,0 +1,9 @@
+# Auto detect text files and perform LF normalization
+* text=auto
+import random
+
+start = int(input("Введите начало диапазона: "))
+end = int(input("Введите конец диапазона: "))
+
+random_number = random.randint(start, end)
+print("Случайное число из диапазона:", random_number)
